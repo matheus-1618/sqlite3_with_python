7d3e89976ed3ff5d48dec5c0a2a536c4115b676a
matheus-1618 <matheus1207live@gmail.com>
2021-03-06 20:19:07 -0300

    sorting by order

---
+++ SQL_files/sort_order_by.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM CLOTHES1
+ORDER BY Type;
+--Takes the name of one or more columns
+--Add a comma after each addtional column name
+--Can sort by a column not retrieved
+--Must always be the last clause in a select statement
+--DESC descending order
+--DESC e ASC asceding order
